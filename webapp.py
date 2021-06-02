308e0732f652d48ae5a8774395cb6d14ace59d6d
Flan-231 <77691192+Flan-231@users.noreply.github.com>
2021-06-01 22:33:09 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -24,12 +24,8 @@ def startOver():
 
 @app.route('/page1')
 def renderPage1():
-    return render_template('page1.html')
-
-@app.route('/page2',methods=['GET','POST'])
-def renderPage2():
-    session["Answer"]=request.form['Answer']
-    if Answer == 'The overall temperature of the ocean increases':
+   session["Answer"]=request.form['Answer']
+   if Answer == 'The overall temperature of the ocean increases':
         reply = "Your answer is correct!"
     elif Answer == 'The overall temperature of the coean decreases':
         reply = "Incorrect"  
@@ -37,6 +33,12 @@ def renderPage2():
         reply = "Incorrect"
     else:
       reply=" Please make sure you've typed the answer in correctly"
+    return render_template('page1.html')
+
+@app.route('/page2',methods=['GET','POST'])
+def renderPage2():
+    session["Answer"]=request.form['Answer']
+   
       
     return render_template('page2.html')
 
