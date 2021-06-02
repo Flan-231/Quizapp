d18bc6aa00c55e6979d529cbcec59b7661f79e60
Flan-231 <77691192+Flan-231@users.noreply.github.com>
2021-06-01 22:39:56 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -29,14 +29,14 @@ def renderPage1():
 @app.route('/page2',methods=['GET','POST'])
 def renderPage2():
     session["Answer"]=request.form['Answer']
-   if Answer == 'The overall temperature of the ocean increases':
+    if Answer == 'The overall temperature of the ocean increases':
         reply = "Your answer is correct!"
-   elif Answer == 'The overall temperature of the coean decreases':
+    elif Answer == 'The overall temperature of the coean decreases':
         reply = "Incorrect"  
-   elif Answer == 'More ice blocks are formed.':
+    elif Answer == 'More ice blocks are formed.':
         reply = "Incorrect"
-   else:
-    reply=" Please make sure you've typed the answer in correctly"
+    else:
+      reply=" Please make sure you've typed the answer in correctly"
    
       
     return render_template('page2.html')
